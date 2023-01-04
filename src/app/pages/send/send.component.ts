b547714ca7c4bdd94f0bf5b77b4028563de909a8
Marviel Billante <marvielb@gmail.com>
2023-01-04 11:05:49 +0800

    send simple layout

--- src/app/pages/send/send.component.ts
+++ src/app/pages/send/send.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-send',
   templateUrl: './send.component.html',
-  styleUrls: ['./send.component.scss']
+  styleUrls: ['./send.component.scss'],
 })
-export class SendComponent {
-
-}
+export class SendComponent {}
