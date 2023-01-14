882bd7f88d7f25aecc7970c92434ed1d60afbbe9
Marviel Billante <marvielb@gmail.com>
2023-01-14 11:07:13 +0800

    saving

--- src/app/pages/template/template-sidebar/template-sidebar.component.ts
+++ src/app/pages/template/template-sidebar/template-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { SideBarService } from 'src/app/components/side-bar/side-bar.service';
+import { Template } from '../template.model';
 import { TemplateService } from '../template.service';
 
 @Component({
@@ -8,6 +10,10 @@ import { TemplateService } from '../template.service';
   styleUrls: ['./template-sidebar.component.scss'],
 })
 export class TemplateSidebarComponent {
+  templates$: Observable<Array<Template>> =
+    this.templateService.templates$.pipe(
+      map((t) => t.sort((t1, t2) => (t1.name < t2.name ? -1 : 1)))
+    );
   constructor(
     protected sideBarService: SideBarService,
     protected templateService: TemplateService
--- src/app/pages/template/template.service.ts
+++ src/app/pages/template/template.service.ts
@@ -10,29 +10,37 @@ export class TemplateService {
   public templates$: Observable<Array<Template>>;
 
   constructor() {
-    this._templates$ = new BehaviorSubject<Array<Template>>([]);
+    const savedTemplates: Array<Template> = localStorage.getItem('templates')
+      ? JSON.parse(localStorage.getItem('templates')!)
+      : [];
+    this._templates$ = new BehaviorSubject<Array<Template>>(savedTemplates);
     this.templates$ = this._templates$.asObservable().pipe(shareReplay(1));
-    this._templates$.next([
-      {
-        id: '1',
-        name: 'Request for Connection',
-        defaultTo: 'it@example.com',
-        defaultCC: '',
-        fields: [{ id: 'date_today', name: 'Date Today' }],
-        body: 'Hi, programmers department would like to request for a connection on {{ date_today }} for enhancements',
-      },
-      {
-        id: '2',
-        name: 'Request for Backup',
-        defaultTo: 'it2@example.com',
-        defaultCC: '',
-        fields: [
-          { id: 'date_today', name: 'Date Today' },
-          { id: 'database_name', name: 'Database Name' },
-        ],
-        body: 'Hi, programmers department would like to request for a backup on {{ date_today }} with the database name: {{ database_name }}',
-      },
-    ]);
+    if (savedTemplates.length == 0) {
+      this._templates$.next([
+        {
+          id: '1',
+          name: 'Request for Connection',
+          defaultTo: 'it@example.com',
+          defaultCC: '',
+          fields: [{ id: 'date_today', name: 'Date Today' }],
+          body: 'Hi, programmers department would like to request for a connection on {{ date_today }} for enhancements',
+        },
+        {
+          id: '2',
+          name: 'Request for Backup',
+          defaultTo: 'it2@example.com',
+          defaultCC: '',
+          fields: [
+            { id: 'date_today', name: 'Date Today' },
+            { id: 'database_name', name: 'Database Name' },
+          ],
+          body: 'Hi, programmers department would like to request for a backup on {{ date_today }} with the database name: {{ database_name }}',
+        },
+      ]);
+    }
+    this.templates$.subscribe((template) =>
+      localStorage.setItem('templates', JSON.stringify(template))
+    );
   }
 
   updateTemplate(id: string, template: Template) {
@@ -43,6 +51,14 @@ export class TemplateService {
           ...templates.filter((t) => t.id != id),
           template,
         ])
+      );
+  }
+
+  deleteTemplate(id: string) {
+    this.templates$
+      .pipe(take(1))
+      .subscribe((templates) =>
+        this._templates$.next(templates.filter((t) => t.id != id))
       );
   }
 }
