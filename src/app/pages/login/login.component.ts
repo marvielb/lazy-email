35542dbd61bc07db8f5d355d1b4037b3c0d75891
Marviel Billante <marvielb@gmail.com>
2023-01-23 15:12:15 +0800

    google integration

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  userToken$: Subject<string> = new Subject();
+  btnEnabled$: Observable<boolean> = this.authService.tokenClientLoaded$;
 
+  constructor(
+    protected authService: AuthService,
+    protected applicationRef: ApplicationRef
+  ) {}
+
+  onClick() {
+    this.authService.login();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,56 @@
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Injectable, NgZone } from '@angular/core';
+import {
+  map,
+  Observable,
+  ReplaySubject,
+  shareReplay,
+  Subject,
+  take,
+} from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  public user$: Observable<any> = new Subject();
+  private _gapiClientLoaded$: ReplaySubject<void> = new ReplaySubject();
 
-  constructor() {}
+  private _tokenClient$: Observable<google.accounts.oauth2.TokenClient> =
+    this._gapiClientLoaded$.pipe(
+      map(() =>
+        google.accounts.oauth2.initTokenClient({
+          client_id: environment.googleClientId,
+          scope: 'https://www.googleapis.com/auth/gmail.readonly',
+          callback: (tokenResponse: google.accounts.oauth2.TokenResponse) => {
+            console.log(tokenResponse);
+          },
+        })
+      ),
+      shareReplay(1)
+    );
+
+  public user$: Subject<any> = new Subject();
+  public tokenClientLoaded$: Observable<boolean> = this._tokenClient$.pipe(
+    map(() => true)
+  );
+
+  public login() {
+    this._tokenClient$
+      .pipe(take(1))
+      .subscribe((tokenClient) =>
+        tokenClient.requestAccessToken({ prompt: 'consent' })
+      );
+  }
+
+  constructor(private ngZone: NgZone) {
+    gapi.load('client', async () => {
+      await gapi.client.init({
+        apiKey: environment.googleApiKey,
+        discoveryDocs: [environment.gmailDiscoveryDoc],
+      });
+      this.ngZone.run(() => {
+        this._gapiClientLoaded$.next();
+      });
+    });
+  }
 }
