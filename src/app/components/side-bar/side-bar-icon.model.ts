2fa661ce302b35b0b49fc744ceb80949882902ae
marvielb <marvielb@users.noreply.github.com>
2023-11-30 13:17:57 +0000

    deploy: 372b9154c44d352105d65c823a5f2afd89ed3e00

--- src/app/components/side-bar/side-bar-icon.model.ts
+++
@@ -1,6 +0,0 @@
-export interface SideBarIcon {
-  svgPath: string;
-  viewBox: string;
-  urlPath: string;
-  tooltip: string;
-}
--- src/app/pages/template/field.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Field {
-  id: string;
-  name: string;
-}
